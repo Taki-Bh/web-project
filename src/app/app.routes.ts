5ff103daa4be5e9af2b20a1235d05dea47009764
Taki-bh <baccouchetaki@gmail.com>
2025-11-25 13:19:40 +0100

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,33 @@
+import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Staff } from './staff/staff';
+import { Content } from './content/content';
+import { Offers } from './offers/offers';
+import { Articles } from './articles/articles';
+import { Home } from './home/home';
+
+export const routes: Routes = [
+    {
+        path: "about",
+        component:About
+    },
+    {
+        path: "staff",
+        component: Staff
+    },
+    {
+        path: "content",
+        component: Content
+    },
+    {
+        path: "offers",
+        component: Offers
+    },{
+        path: "articles",
+        component: Articles
+    },
+    {
+        path: "home",
+        component: Home
+    }
+];
