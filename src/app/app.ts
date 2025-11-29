9adcb67d4d2052c5342644f0c9d8176c09c9a540
Taki-bh <baccouchetaki@gmail.com>
2025-11-29 18:26:10 +0100

    fourth commit/ slide animation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLinkActive, RouterLink } from '@angular/router';
 
+import { Component, OnInit, signal } from '@angular/core';
+import { RouterOutlet, RouterLinkActive, RouterLink } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLinkActive,RouterLink],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  
 })
-export class App {
+
+export class App{
+
+
+  loaded :boolean=false;
+  onActivate() {
+    this.loaded=false;
+    alert("hi");
+               // reset state
+
+    setTimeout(() => this.loaded = true, 10); // apply loaded state
+  
+}
   protected readonly title = signal('web-project');
 }
