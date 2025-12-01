789f1f90eda3d8c1396dec5d0d063915725cc897
Taki-bh <baccouchetaki@gmail.com>
2025-12-01 11:33:29 +0100

    added stuff

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,34 @@
-
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, HostListener, signal, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet, RouterLinkActive, RouterLink } from '@angular/router';
+import { NgStyle} from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLinkActive,RouterLink],
+  imports: [RouterOutlet, RouterLinkActive, RouterLink,NgStyle],
   templateUrl: './app.html',
+  
   styleUrl: './app.css',
   
 })
-
-export class App{
-
-
-  loaded :boolean=false;
+export class App {
+  platformIsClicked = false;
+  loaded: boolean = false;
   onActivate() {
-    this.loaded=false;
-               // reset state
+    this.loaded = false;
+    // reset state
 
-    setTimeout(() => this.loaded = true, 10); // apply loaded state
-  
-}
+    setTimeout(() => (this.loaded = true), 10); // apply loaded state
+  }
+  onPlatformClick() {
+    alert(this.platformIsClicked);
+    this.platformIsClicked = false;
+    setTimeout(() => (this.platformIsClicked = true), 10);
+  }
+  @HostListener('document:click', ['$event'])
+  handleKeyDown(event: MouseEvent) {
+    
+
+
+  }
+   
   protected readonly title = signal('web-project');
 }
