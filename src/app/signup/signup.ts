910e03a1fa5accfa060e27f6bb1fcc79179d451d
rayenbouajila <rayenbouajila19@gmail.com>
2025-12-11 14:04:11 +0100

    signup updated , conditions

--- src/app/signup/signup.ts
+++ src/app/signup/signup.ts
@@ -1,35 +1,33 @@
 import { Component } from '@angular/core';
-import {ReactiveFormsModule,FormGroup,FormControl,Validators,AbstractControl,ValidationErrors} from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './signup.html',
   styleUrls: ['./signup.css'],   
 })
 export class Signup {
+  signupform = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.required])
+  }, {
+    validators: [Signup.passwordsMatchValidator]
+  });
 
-  signupform = new FormGroup(
-    {
-      username: new FormControl('', [Validators.required,Validators.minLength(3)]),
-      email: new FormControl('', [Validators.required,Validators.email]),
-      password: new FormControl('', [Validators.required,Validators.minLength(6)]),
-      confirmPassword: new FormControl('', [Validators.required])
-    },
-    {
-      validators: [Signup.passwordsMatchValidator] 
-    }
-  );
-  static passwordsMatchValidator(
-    control: AbstractControl
-  ): ValidationErrors | null {
+  static passwordsMatchValidator(control: AbstractControl): ValidationErrors | null {
     const password = control.get('password')?.value;
     const confirm = control.get('confirmPassword')?.value;
     if (password !== confirm) {
-      return { passwordsMismatch: true };   
+      return { passwordsMismatch: true };
     }
-    return null;  
+    return null;
   }
+
   submit() {
     if (this.signupform.valid) {
       console.log("User Registered:", this.signupform.value);
