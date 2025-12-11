845c889a2c567e21d7a6a63f3254aba4163f2253
rayenbouajila <rayenbouajila19@gmail.com>
2025-12-11 10:50:59 +0100

    signup updated

--- src/app/signup/signup.ts
+++ src/app/signup/signup.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
-
+import {ReactiveFormsModule,FormGroup,FormControl,Validators,AbstractControl,ValidationErrors} from '@angular/forms';
 @Component({
   selector: 'app-signup',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.html',
-  styleUrl: './signup.css',
+  styleUrls: ['./signup.css'],   
 })
 export class Signup {
 
+  signupform = new FormGroup(
+    {
+      username: new FormControl('', [Validators.required,Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required,Validators.email]),
+      password: new FormControl('', [Validators.required,Validators.minLength(6)]),
+      confirmPassword: new FormControl('', [Validators.required])
+    },
+    {
+      validators: [Signup.passwordsMatchValidator] 
+    }
+  );
+  static passwordsMatchValidator(
+    control: AbstractControl
+  ): ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const confirm = control.get('confirmPassword')?.value;
+    if (password !== confirm) {
+      return { passwordsMismatch: true };   
+    }
+    return null;  
+  }
+  submit() {
+    if (this.signupform.valid) {
+      console.log("User Registered:", this.signupform.value);
+    } else {
+      this.signupform.markAllAsTouched();
+    }
+  }
 }
